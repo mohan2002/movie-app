6b7d68974bf7f7cae8d168498f17a2a0d286c34a
Mohan <mohanprasaath269@gmail.com>
2021-07-01 08:51:38 +0530

    ui commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "d24f8506e7d2348b5e97b0f2cd424f50";
+
+const requests = {
+   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+   fetchDocumantries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
